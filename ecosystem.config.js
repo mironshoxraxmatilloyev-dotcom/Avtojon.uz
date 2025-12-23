2964c1ea6c9bc6217bf5f5e60252bcc8c22b0567
Javohir <jovohijabborov85@egmail.com>
2025-12-23 14:42:34 +0500

    Tayyor

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       exec_mode: 'fork',
       env: {
         NODE_ENV: 'production',
-        PORT: 3000
+        PORT: 5002
       },
       watch: false,
       max_memory_restart: '500M',
